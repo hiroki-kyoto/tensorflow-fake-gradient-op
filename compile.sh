8c1a75b1742da9d3f9d0f75b9cc56b50e480bd51
Hiroki <yamasaki_hiroki_kyoto@yahoo.co.jp>
2018-03-19 14:05:08 +0800

    name fix!!!

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
-g++ -std=c++11 -shared zero_out.cc -o zero_out.so -fPIC -I $TF_INC -O2
+g++ -std=c++11 -shared fake_gradient_op.cc -o fake_gradient_op.so -fPIC -I $TF_INC -O2
